f62c420cb5aa7f03a7058a3052054f2aa41640d0
Jellyton <jellyton69@gmail.com>
2024-08-01 03:19:13 +0000

    fix avatar export

--- server/main.lua
+++ server/main.lua
@@ -264,7 +264,7 @@ RegisterServerEvent('QBCore:Everfall:EMS:Timeclock', function(Player, ClockingIn
 		{
 			['author'] = {
 				['name'] = GetPlayerName(source),
-				['icon_url'] = exports.ef_lib:GetAvatar(source),
+				['icon_url'] = exports.ef_discord:GetPlayerAvatar(source),
 			},
 			['title'] = (ClockingIn and "Clock In") or "Clock Out",
 			['color'] = (ClockingIn and 3858002) or 16068139,
